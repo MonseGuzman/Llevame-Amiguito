fc2bff0389a8dddf5d012964fe3efd1e705e01cb
Monse Guzmán <andreaguzman.lopez23@gmail.com>
2018-11-02 13:29:42 -0600

    ahora si sirve

--- app/src/main/java/com/monse/andrea/proyectofinal/LoginActivity.java
+++ app/src/main/java/com/monse/andrea/proyectofinal/LoginActivity.java
@@ -1,24 +1,37 @@
 package com.monse.andrea.proyectofinal;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener
 {
     private GoogleApiClient googleApiClient;
     private static int CODE_LOGIN = 1;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient googleSignInClient;
 
     private SignInButton SignButton;
 
@@ -29,22 +42,27 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
         iniciar();
 
+        mAuth = FirebaseAuth.getInstance();
         //para una autentificacion con más datos
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail() //trae el correo
                 .build();
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
         //sirve para sincronizar el ciclo de vida de la activity y google
-        googleApiClient = new GoogleApiClient.Builder(this)
+       /* googleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+                .build();*/
 
         //acción del botón
         SignButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                //Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                Intent intent = googleSignInClient.getSignInIntent();
                 startActivityForResult(intent, CODE_LOGIN);
             }
         });
@@ -61,11 +79,51 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == CODE_LOGIN)
+        if (requestCode == CODE_LOGIN)
+        {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("a", "Google sign in failed", e);
+                // ...
+            }
+        }
+        /*if(requestCode == CODE_LOGIN)
         {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             resultadoLogin(result);
-        }
+        }*/
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account)
+    {
+        Log.d("a", "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("a", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            abrirActivity();
+                            Toast.makeText(LoginActivity.this, "lo lograste", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("a", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     private void resultadoLogin(GoogleSignInResult result)
@@ -79,7 +137,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     private void abrirActivity()
     {
         Intent i = new Intent(this, MainActivity.class);
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(i);
     }
 
